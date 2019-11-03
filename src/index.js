169fcdcacc2c74e8df2a8494430ecd276b7280f4
Nick Nist <nicknistoc@gmail.com>
2019-11-03 16:12:36 -0700

    Add customer interaction functionality and page switches

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
+import Chart from 'chart.js';
+import moment from 'moment';
+import datepicker from 'js-datepicker';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import Bookings from './Bookings.js';
+import Customer from './Customer.js';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+
+
+
 
 let userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(response => response.json()).then(data => data.users);
 let roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(response => response.json()).then(data => data.rooms);
 let bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(response => response.json()).then(data => data.bookings);
+
 var booking;
+var currentCustomer;
 let currentDate = findCurrentDate();
 
 Promise.all([userData, roomData, bookingData]).then(data => {
@@ -42,71 +51,178 @@ function checkButtons(event) {
     changeToManagerPage();
   }
   if (event.target.id === 'login-button' && checkLogin() === false) {
-    changeToCustomerPage(getCustomerID());
+    let customerID = getCustomerID();
+    currentCustomer = new Customer(booking.findCustomerBookings(customerID), customerID, userData.find(data => customerID === data.id).name)
+    changeToCustomerPage(currentCustomer.name);
   }
   if (event.target.id === 'logout-button') {
     changeToLoginPage();
+  }
+  if (event.target.id === 'booking-button') {
+    changeToBookingPage();
+  }
+  if (event.target.id === 'find-date') {
+    changeToAvailableRoomsPage();
+  }
+  if (event.target.id === 'filter-rooms') {
+    filterRooms(booking.roomsAvailable(currentCustomer.chosenDate));
+  }
+  if (event.target.id === 'choose-room') {
+    bookRoom();
   }
 }
 
 function changeToManagerPage() {
   $('main').html(`<article class="manager">
     <h2>available rooms:</h2>
-    <ul class="room-list">${getRoomsAvailableData()}</ul>
+    <ul class="room-list">${getRoomsAvailableData(currentDate, '<li>', '</li>')}</ul>
   </article>
   <article class="manager">
     <h2>today's revenue:</h2>
     <p>$${booking.totalRevenue(currentDate)}</p>
   </article>
-  <article class="manager">
+  <article class="manager chart">
     <h2>percent of rooms occupied:</h2>
-    <p>${booking.percentOfRoomsOccupied(currentDate)}%</p>
+    <canvas id="percent-chart"></canvas>
   </article>`);
   $('body').addClass('manager-page');
   $('header').prepend(`<div class="input-box">
     <label for="user-search" class="initial-text">search:</label>
     <input id="user-search" type="text" name="Name Search Input" placeholder="customer name">
+    <button id="search-customer" type="button" class="logout-button find-customer" name="Manager search for a customer button">find</button>
   </div>`);
   $('header').append(`<button id="logout-button" type="button" class="logout-button" name="Logout Button">logout</button>`);
+  getPercentChart();
 }
 
-function getRoomsAvailableData() {
+function getPercentChart() {
+  let roomsAvailable = booking.roomsAvailable(currentDate).length;
+  let totalRooms = booking.roomData.length;
+  new Chart($('#percent-chart'), {
+      type: 'doughnut',
+      data: {
+        labels: ["Available Rooms", "Occupied Rooms"],
+        datasets: [
+          {
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+            data: [roomsAvailable, (totalRooms - roomsAvailable)]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        layout: {
+          padding: {
+            left: 0,
+            right: 0,
+            top: 10,
+            bottom: 10
+          }
+        }
+      }
+  });
+}
+
+function getRoomsAvailableData(date, element, closingElement) {
   let string = '';
-  booking.roomsAvailable(currentDate).forEach(room => {
-    string += `<li>Room #${room.number} is available. It is a ${room.roomType} with a ${room.bedSize.toUpperCase()} bed</li>`
-  })
+  booking.roomsAvailable(date).forEach(room => {
+    string += `${element}Room #${room.number} is available. It is a ${room.roomType} with a ${room.bedSize.toUpperCase()} bed${closingElement}`
+    })
   return string;
 }
 
-function changeToCustomerPage() {
+function changeToCustomerPage(name) {
   $('main').html(`<article class="manager">
     <h2>past bookings:</h2>
-    <ul class="room-list">${booking.findPastCustomerBookings(getCustomerID(), currentDate)}</ul>
+    <ul class="room-list">${getPastDataString()}</ul>
   </article>
   <article class="manager">
     <h2>upcoming bookings:</h2>
-    <p>${booking.findUpcomingCustomerBookings(getCustomerID(), currentDate)}</p>
+    <ul class="room-list">${getUpcomingDataString()}</ul>
   </article>
   <article class="manager">
     <h2>total spent:</h2>
+    <p>You have spent $${booking.totalAmountCustomerSpent(getCustomerID())} at Nister's Tripster for Hipsters</p>
   </article>`);
   $('body').addClass('customer-page');
-  $('header').prepend(`<button id="booking-button" type="button" name="Customer Booking">new booking</button>`);
-  $('header').append(`<button id="logout-button" type="button" class="logout-button" name="Logout Button">logout</button>`);
+  $('header').html(`<button id="booking-button" type="button" name="Customer Booking" class="header-booking">new booking</button>
+  <h1>welcome ${name}</h1>
+  <button id="logout-button" type="button" class="logout-button" name="Logout Button">logout</button>`);
 }
 
 function getPastDataString() {
+  //in the future -> sort the bookings then put them into a more read-able format
   let string = '';
-  booking.findPastCustomerBookings(getCustomerID(), currentDate).forEach(book => {
-    string += `<li>You have booked room #${book.roomNumber} on ${book.date}</li>`
+  booking.findPastCustomerBookings(getCustomerID(), currentDate).forEach(pastBooking => {
+    string += `<li>You have booked room #${pastBooking.roomNumber} on ${pastBooking.date}</li>`
   });
-  console.log(string);
+  return string;
+}
+
+function getUpcomingDataString() {
+  let string = '';
+  booking.findUpcomingCustomerBookings(getCustomerID(), currentDate).forEach(upcomingBooking => {
+    string += `<li>You have booked room #${upcomingBooking.roomNumber} on ${upcomingBooking.date}</li>`
+  });
   return string;
 }
 
 function getCustomerID() {
   //this will need to be dynamic eventually and get the current customerID - possibly just make a global variable cuz yolo
   return 30;
+}
+
+function changeToBookingPage() {
+  $('main').html(`<article class="initial-login">
+    <div id="date-box" class="input-box">
+      <label for="date-input" class="initial-text">date:</label>
+      <input id="date-input" type="text" name="Date Input" placeholder="date">
+    </div>
+    <button id="find-date" type="button" class="login-button" name="Find Date for Booking">find date</button>
+  </article>`);
+  $('header').html(`<h1>welcome ${currentCustomer.name}, choose a date then a room to book</h1>`);
+  const picker = datepicker('#date-input', {
+    position: 'tl',
+    customDays: ['S', 'M', 'T', 'W', 'Th', 'F', 'S'],
+    customMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    overlayButton: 'Choose',
+    dateSelected: new Date(),
+    onSelect: (instance, date) => {
+      currentCustomer.chosenDate = (moment(date).format('YYYY/MM/DD'));
+    }
+  });
+  $('.qs-datepicker').css('display', 'inline-block');
+  picker.show();
+}
+
+function changeToAvailableRoomsPage() {
+  let openingElement = `<div><input name="room" type="radio">`;
+  if (booking.roomsAvailable(currentCustomer.chosenDate).length === 0) {
+    $('main').html(`<article class="initial-login">
+    <p>Sorry! There are no rooms available on that date</p>
+    <button id="booking-button" type="button" name="Customer Booking" class="login-button go-back">back to new booking</button>
+    </article>`);
+  } else {
+    $('main').html(`<article class="initial-login">
+    <label for="date-input" class="initial-text">rooms available:</label>
+    <ul class="room-list" id="rooms-available">${getRoomsAvailableData(currentCustomer.chosenDate, openingElement, '</div>')}</ul>
+    <div>
+      <button id="choose-room" type="button" class="login-button" name="Choose the room you want">book now</button>
+      <button id="filter-rooms" type="button" class="login-button" name="Choose the room you want">filter rooms</button>
+    </div>
+    <button id="booking-button" type="button" name="Customer Booking" class="login-button go-back">back to new booking</button>
+    </article>`);
+  }
+}
+
+function filterRooms(roomsAvailable) {
+  let string = '';
+  currentCustomer.filterRoomsByType(roomsAvailable).forEach(room => {
+    string += `<div><input name="room" type="radio">Room #${room.number} is available. It is a ${room.roomType} with a ${room.bedSize.toUpperCase()} bed</div>`
+  })
+  $('#rooms-available').html(`${string}`);
 }
 
 function changeToLoginPage() {
@@ -126,6 +242,18 @@ function changeToLoginPage() {
   $('header').html(`<h1>NISTER'S TRIPSTER FOR HIPSTERS</h1>`);
   $('#username-box').append(`<p id="username-check" class="hidden">please enter a valid username</p>`);
   $('#password-box').append(`<p id="password-check" class="hidden">please enter a valid password</p>`);
+}
+
+function bookRoom() {
+  let text = $('input[name="room"]:checked').closest('div').text()
+  let split = text.split(' ');
+  let roomNumber = split[1][1];
+  if (split[1][2]) {
+    roomNumber += split[1][2];
+  }
+  currentCustomer.bookRoom(currentCustomer.chosenDate, parseInt(roomNumber));
+  changeToCustomerPage();
+  $('header').append(`<br><h1>Congrats! You Booked the Room!</h1>`);
 }
 
 function checkLogin() {
